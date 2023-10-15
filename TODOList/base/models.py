0fd5a1da38956faff82c46ab13c31d5028878e09
Sanath <sanath50@gmail.com>
2023-10-15 20:32:03 +0530

    Todo List

--- TODOList/base/models.py
+++ TODOList/base/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self): #order by title, title attribute is the default
+        return self.title
+
+    class Meta: #set the default ordering, default order
+        ordering = ['complete']
